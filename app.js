c3bface4487a0870bcd820ebaa01f15c89fc459f
EdgarG1013 <edgarstiven46@gmail.com>
2025-09-19 16:08:37 -0500

    feat(app.js): se agrego la primera funcion del desafio [añadir amigos y verificar campos]
    Se añadio la primera funcion en app.js para agregar los nombres al arreglo.
    Se añadio a la funcion agregarAmigo se le agregaron condiciones para verificar que el campo de texto no este vacio o repetido.

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Lista que almacena los nombres añadidos
+let amigos = [];
+
+
+// Función para añadir un nombre a la lista
+function agregarAmigo() {
+
+    let ingresarAmigo = document.getElementById('amigo');
+	let nombre = ingresarAmigo.value.trim();
+
+	// Verficacion de que el campo no este vacío
+	if (!nombre) {
+		alert('Por favor ingresa un nombre válido.');
+		ingresarAmigo.focus();
+		return;
+	}
+
+	// Verificacion de que el nombre no este repetido
+	if (amigos.includes(nombre)) {
+		alert('Ese nombre ya fue añadido.');
+		ingresarAmigo.value = '';
+		ingresarAmigo.focus();
+		return;
+	}
+
+	amigos.push(nombre);
+	ingresarAmigo.value = '';
+	ingresarAmigo.focus();
+
+
+    console.log(amigos);
+}
+
+// Funcion para actualizar la lista en el HTML
+function actualizarLista() {
+
+
+}
